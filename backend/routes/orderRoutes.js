9afc3fa2d5ebe78aa5980004877f7ce99de58024
AlexKasema <lexxmulwa@gmail.com>
2024-03-15 15:15:38 +0300

    Created orderRoutes and orderController for the orders. And set all the routes in postman.

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,25 @@
+import express from 'express';
+const router = express.Router();
+import { 
+    addOrderItems,
+    getMyOrders,
+    getOrderById,
+    updateOrderToPaid,
+    updateOrderToDelivered,
+    getOrders,
+} from '../controllers/orderController.js';
+
+import { protect, admin } from '../middleware/authMiddleware.js';
+
+
+router.route('/').get(protect, admin, getOrders).post(protect, addOrderItems);
+
+router.route('/mine').get(protect, getMyOrders);
+router.route('/:id/pay').put(protect, updateOrderToPaid);
+
+
+//admin
+router.route('/:id').get(protect, admin, getOrderById);
+router.route('/:id/deliver').put(protect, admin , updateOrderToDelivered);
+
+export default router;
