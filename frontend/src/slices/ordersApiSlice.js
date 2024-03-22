831374bfffa55c2af0f2ee079779e54457f58008
AlexKasema <lexxmulwa@gmail.com>
2024-03-23 00:51:52 +0300

    Implemented display orders functionality whereby users can view their orders in the profile

--- frontend/src/slices/ordersApiSlice.js
+++ frontend/src/slices/ordersApiSlice.js
@@ -28,6 +28,12 @@ export const ordersApiSlice = apiSlice.injectEndpoints({
                 url: PAYPAL_URL,
             }),
             keepUnusedDataFor: 5,
+        }),
+        getMyOrders: builder.query({
+            query: () => ({
+                url: `${ORDERS_URL}/mine`
+            }),
+            keepUnusedDataFor: 5,
         })
     }),
 });
@@ -37,4 +43,5 @@ export const {
     useGetOrderDetailsQuery,
     usePayOrderMutation,
     useGetPayPalClientIdQuery,
+    useGetMyOrdersQuery,
  } = ordersApiSlice;
